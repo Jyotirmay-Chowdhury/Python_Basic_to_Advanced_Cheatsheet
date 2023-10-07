72524241fc53ed8c80b1774ef9c531bd815c7f7e
Jyotirmay Chowdhury <chowdhuryjyobad21hbcs@student.mes.ac.in>
2023-10-07 22:01:38 +0530

    Add files via upload

---
+++ Python_Syntax_&_Comments.py
@@ -0,0 +1,8 @@
+# Syntax:- print("Hello, world!")
+print("Hello, Guys")
+
+#Comments like Single line comments and Multiline comments
+
+'''Single Multi-line comments'''
+
+"""Double Multi-line comments"""
